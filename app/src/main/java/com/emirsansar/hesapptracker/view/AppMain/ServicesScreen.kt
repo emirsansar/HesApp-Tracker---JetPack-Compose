e58ce1e8b781b92e9e0cce4a4e0e07338b282c35
Emir Sansar <emirsansar@hotmail.com>
2024-10-21 14:26:49 +0300

    "Removed custom service addition button from the ServiceScreen and integrated it into the newly added TopAppBar."

--- app/src/main/java/com/emirsansar/hesapptracker/view/AppMain/ServicesScreen.kt
+++ app/src/main/java/com/emirsansar/hesapptracker/view/AppMain/ServicesScreen.kt
@@ -1,18 +1,30 @@
 package com.emirsansar.hesapptracker.view.AppMain
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,13 +33,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.emirsansar.hesapptracker.R
 import com.emirsansar.hesapptracker.model.Service
 import com.emirsansar.hesapptracker.viewModel.ServiceViewModel
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ServicesScreen(
     modifier: Modifier, serviceVM:
@@ -35,7 +52,6 @@ fun ServicesScreen(
     navController: NavController
 ){
     var serviceList by remember { mutableStateOf(emptyList<Service>()) }
-
     val context = LocalContext.current
 
     LaunchedEffect(Unit) {
@@ -44,17 +60,20 @@ fun ServicesScreen(
         }
     }
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Services", fontSize = 30.sp)
-
-        AddCustomServiceButton(context)
-
-        ServiceList(serviceList, navController)
-    }
+    Scaffold(
+        topBar = { TopBarServiceScreen(context) },
+        content = { paddingValues ->
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally)
+            {
+                ServiceList(serviceList, navController)
+            }
+        }
+    )
 
 }
 
@@ -94,29 +113,20 @@ private fun ServiceCard(service: Service, navController: NavController) {
 }
 
 @Composable
-private fun AddCustomServiceButton(context: Context) {
-    Card(
-        modifier = Modifier
-            .padding(all = 20.dp)
-            .height(50.dp)
-            .fillMaxWidth()
-            .clickable {
+private fun TopBarServiceScreen(context: Context) {
+    TopAppBar(
+        title = {
+            Text(text = "Services", fontSize = 21.sp, fontWeight = FontWeight.SemiBold)
+        },
+        actions = {
+            IconButton(onClick = {
                 val intent = Intent(context, CustomServiceScreen::class.java)
                 context.startActivity(intent)
+            }) {
+                Icon( imageVector = Icons.Default.AddCircle, contentDescription = "Add Service Button", tint = Color.DarkGray )
             }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(all = 2.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.Start
-        ) {
-            Text(
-                text = "Add Custom Service",
-                fontSize = 20.sp,
-                modifier = Modifier.padding(start = 5.dp)
-            )
-        }
-    }
+        },
+        backgroundColor = Color.LightGray,
+        modifier = Modifier.fillMaxWidth()
+    )
 }
