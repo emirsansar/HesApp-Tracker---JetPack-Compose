cfc026a7f1b2c1d39234a204308f4a954db21426
Emir Sansar <emirsansar@hotmail.com>
2024-10-26 21:26:09 +0300

    Colors file has been updated with colors for composables according to light and dark themes.

--- app/src/main/java/com/emirsansar/hesapptracker/ui/theme/Color.kt
+++ app/src/main/java/com/emirsansar/hesapptracker/ui/theme/Color.kt
@@ -10,4 +10,19 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val AppBackgroundColor = Color(0xff9ed8e8)
+
+// Dark Theme
+object DarkThemeColors {
+    val BarColor = Color(0xFF414141)
+    val BackgroundColor = Color(0xFF525252)
+    val CardColorHomeScreen = Color(0xFF2384D4)
+    val DrawerContentColor = Color(0xFF4A4A4A)
+}
+
+// Light Theme
+object LightThemeColors {
+    val BarColor = Color(0xFF7FA1C3)
+    val BackgroundColor = Color(0xFFcad9e8)
+    val CardColorHomeScreen = Color(0xFF7FA1C3)
+    val DrawerContentColor = Color(0xFFA4BDD6)
+}
