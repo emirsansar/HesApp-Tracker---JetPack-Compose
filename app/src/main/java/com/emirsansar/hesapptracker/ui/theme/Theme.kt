5b147748129f0187479752c513a5f6ba16f6e633
Emir Sansar <emirsansar@hotmail.com>
2024-11-04 13:49:11 +0300

    Updated the color schemes for both dark and light themes in the application, ensuring that the primary color represents the status bar color.

--- app/src/main/java/com/emirsansar/hesapptracker/ui/theme/Theme.kt
+++ app/src/main/java/com/emirsansar/hesapptracker/ui/theme/Theme.kt
@@ -10,26 +10,33 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.emirsansar.hesapptracker.manager.AppManager
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkThemeColors.BarColor,
+    secondary = Color.White,
+    tertiary = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightThemeColors.BarColor,
+    secondary = Color.Black,
+    tertiary = Color.Black,
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    onTertiary = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
@@ -44,21 +51,20 @@ fun HesAppTrackerTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val context = LocalContext.current
+    val appManager = AppManager.getInstance(context)
 
-        darkTheme -> DarkColorScheme
+    val colorScheme = when {
+        appManager.isDarkMode.value -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !appManager.isDarkMode.value
         }
     }
 
